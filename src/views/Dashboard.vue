3c5e7c6846c1bf06b3cef3cddf2f1f424a37e462
OussamaIdbid <57132225+OussamaIdbid@users.noreply.github.com>
2021-02-20 18:13:59 +0100

    improved responsiveness of dashboard

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,55 +1,59 @@
 <template>
-    <div>
-        <section class="section is-medium">
-            <div class="columns is-centered">
-                <div class="column is-half">
-                    <h3 class="is-size-3"><strong>Account overzicht</strong></h3>
-                </div>
-            </div>
-            <EditUser />
-            <EditPassword />
-            <!--<ReservationsTable />-->
-        </section>
-
-    </div>
+  <div>
+    <section class="section is-medium">
+      <div class="columns is-centered">
+        <div class="column is-half">
+          <h3 class="is-size-3"><strong>Account overzicht</strong></h3>
+        </div>
+      </div>
+      <EditUser />
+      <EditPassword />
+      <!--<ReservationsTable />-->
+    </section>
+  </div>
 </template>
 
 <script>
-    import User from '../Api/User'
-    import EditUser from '../components/auth/EditUser.vue'
-    import EditPassword from '../components/auth/EditPassword'
-    //import ReservationsTable from '../components/auth/ReservationsTable.vue'
-    export default {
-        name: 'Dashboard',
-        data() {
-            return {
-                user: null
-            }
-        },
-        components: {
-            EditUser,
-            EditPassword,
-            //ReservationsTable
-        },
-        mounted() {
-            User.auth().then(response => {
-                console.log(response.data)
-                this.user = response.data;
-            })
-
-        },
-        methods: {
-            edit(){
-                User.edit().then(response => {
-                    console.log(response);
-                })
-            }
-        }
-    }
+import User from "../Api/User";
+import EditUser from "../components/auth/EditUser.vue";
+import EditPassword from "../components/auth/EditPassword";
+//import ReservationsTable from '../components/auth/ReservationsTable.vue'
+export default {
+  name: "Dashboard",
+  data() {
+    return {
+      user: null,
+    };
+  },
+  components: {
+    EditUser,
+    EditPassword,
+    //ReservationsTable
+  },
+  mounted() {
+    User.auth().then((response) => {
+      console.log(response.data);
+      this.user = response.data;
+    });
+  },
+  methods: {
+    edit() {
+      User.edit().then((response) => {
+        console.log(response);
+      });
+    },
+  },
+};
 </script>
 
 <style scoped>
-    h3 {
-        text-align: center;
+h3 {
+  text-align: center;
+}
+/*Mobile breakpoints*/
+@media only screen and (max-width: 768px) {
+    .section,.container {
+        padding: 0 !important;
     }
+}
 </style>
