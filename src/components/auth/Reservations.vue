72af1dc30f690333e606af94034363a9d3d8e49c
OussamaIdbid <57132225+OussamaIdbid@users.noreply.github.com>
2021-02-22 22:50:48 +0100

    added tabs to reservation page

--- src/components/auth/Reservations.vue
+++ src/components/auth/Reservations.vue
@@ -1,13 +1,19 @@
 <template>
   <div>
-    <div class="columns is-centered is-flex-direction-column">
-      <reservation-card
-        v-for="item in this.data"
-        :key="item.id"
-        v-bind:OrderID="item.id"
-      ></reservation-card>
-    </div>
-
+    
+      <b-tabs v-model="activeTab" position="is-centered" :animated="false" class="block">
+        <b-tab-item label="Reserveringen">
+          <reservation-card
+            v-for="item in this.data"
+            :key="item.id"
+            v-bind:OrderID="item.id"
+          ></reservation-card>
+        </b-tab-item>
+        <b-tab-item label="Geschiedenis">
+          Geen reserveringen
+        </b-tab-item>
+      </b-tabs>
+    
     <b-loading
       :is-full-page="true"
       v-model="isLoading"
@@ -29,11 +35,12 @@ export default {
     return {
       data: null,
       isLoading: true,
+      activeTab:0
     };
   },
   mounted() {
     User.auth().then((response) => {
-      if (response.data.user_type == 2) {
+      if (response.data.user_type == 3) {
         Reservations.getAllReservations().then((response) => {
           this.data = response.data;
           this.isLoading = false;
@@ -52,7 +59,23 @@ export default {
 
 <style scoped>
 .columns {
-    flex-direction: column;
-    align-items: center;
+  flex-direction: column;
+  align-items: center;
+}
+.tab-item {
+
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+}
+/*Mobile breakpoints*/
+@media only screen and (max-width: 768px) {
+    section {
+      padding: 0 !important;
+     
+    }
+
+
 }
 </style>
