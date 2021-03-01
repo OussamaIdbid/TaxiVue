27e6ebdfe3a854cdb4e1630b5e79181269ba3da6
OussamaIdbid <57132225+OussamaIdbid@users.noreply.github.com>
2021-03-01 23:22:04 +0100

    seperate files for constants per category

---
+++ src/variables/promises.js
@@ -0,0 +1,8 @@
+const {
+        PromiseStatuses,
+        PROMISE_RESOLVED,
+        promiseStatus,
+        promiseState,
+        isPromiseResolved,
+        isPromiseNotRejected 
+    } = require('promise-status-async');
---
+++ src/variables/roles.js
@@ -0,0 +1,9 @@
+export const CUSTOMER = 1;
+export const DRIVER = 2;
+export const ADMIN = 3;
+
+export const ROLES = {
+    CUSTOMER: 1,
+    DRIVER: 2,
+    ADMIN: 3
+}
