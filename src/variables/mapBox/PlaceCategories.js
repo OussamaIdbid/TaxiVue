46ec94fdfddb2ec658e18ac3dc64d815622c2af4
OussamaIdbid <57132225+OussamaIdbid@users.noreply.github.com>
2021-03-08 03:01:59 +0100

    added constants for mapbox place category icons in autocomplete

---
+++ src/variables/mapBox/PlaceCategories.js
@@ -0,0 +1,9 @@
+export const CATEGORIES = {
+    ADDRESS: {name: 'address', icon_name: 'home'},
+    AIRPORT: {name: 'airport' , icon_name: 'plane'},
+    SUPERMARKET: {name:"supermarket, shop, groceries, grocery, market, super", icon_name: 'shopping-basket'},
+    RESTAURANT: { name: "restaurant, food", icon_name: 'utensils'},
+    HISTORIC_SITE: { name: 'historic site, historic', icon_name: 'landmark'},
+    GENERAL: {name: 'point', icon_name: 'map-marker'}
+}
+
