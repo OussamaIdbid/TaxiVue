952dca34b7ce725a3582962e95801734b3c56da7
OussamaIdbid <57132225+OussamaIdbid@users.noreply.github.com>
2021-03-15 20:32:06 +0100

    formatted code

--- src/router/index.js
+++ src/router/index.js
@@ -1,145 +1,146 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
-import NotFound from '../components/layouts/NotFound.vue'
-import store from './../Store'
+import Vue from "vue";
+import VueRouter from "vue-router";
+import Home from "../views/Home.vue";
+import NotFound from "../components/layouts/NotFound.vue";
+import store from "./../Store";
 
-Vue.use(VueRouter)
+Vue.use(VueRouter);
 
-  const routes = [
+const routes = [
   {
-    path: '/',
-    name: 'Home',
-    component: Home
+    path: "/",
+    name: "Home",
+    component: Home,
   },
   {
-    path: '/about',
-    name: 'About',
-    component: () => import('../views/About.vue')
+    path: "/about",
+    name: "About",
+    component: () => import("../views/About.vue"),
   },
   {
-    path: '/FareCalculationResult',
-    name: 'FareCalculationResult',
-    component: () => import('../components/FareCalculationResult.vue'),
+    path: "/FareCalculationResult",
+    name: "FareCalculationResult",
+    component: () =>
+      import("../components/reservation/FareCalculationResult.vue"),
     // meta: {CalculatedOnly: true}
-
   },
   {
-    path: '/Reserveer',
-    name: 'ReservationForm',
-    component: () => import('../components/ReservationForm.vue')
+    path: "/Reserveer",
+    name: "ReservationForm",
+    component: () => import("../components/ReservationForm.vue"),
     // props:true,
     // meta: {CalculatedOnly: true}
+  },
+  {
+    path: "/login",
+    name: "Login",
+    meta: { guestOnly: true },
+    component: () => import("../views/Login.vue"),
+  },
+  {
+    path: "/dashboard",
+    name: "Dashboard",
+    component: () => import("../views/Dashboard.vue"),
+    meta: { authOnly: true },
+  },
+  {
+    path: "/register",
+    name: "Register",
+    meta: { guestOnly: true },
+    component: () => import("../views/Register.vue"),
+  },
+  {
+    path: "/verify-email",
+    name: "verifyEmail",
+    meta: { guestOnly: true },
+    component: () => import("../components/auth/VerifyEmail.vue"),
+  },
+  {
+    path: "/forgot-password",
+    name: "forgotPassword",
+    meta: { guestOnly: true },
+    component: () => import("../components/auth/ForgotPassword.vue"),
+  },
+  {
+    path: "/reset-password",
+    name: "resetPassword",
+    meta: { guestOnly: true },
+    component: () => import("../components/auth/ResetPassword.vue"),
+  },
+  {
+    path: "/payment",
+    name: "PaymentResult",
+    meta: { authOnly: true },
+    component: () => import("../components/payment/PaymentResult.vue"),
+  },
+  {
+    path: "/reservations",
+    name: "Reservations",
+    meta: { authOnly: true },
+    component: () => import("../views/Reservations.vue"),
+  },
+  { path: "*", name: "notFound", component: NotFound },
 
-  },
   {
-    path: '/login',
-    name: 'Login',
-    meta: { guestOnly: true},
-    component: () => import('../views/Login.vue')
+    path: "/testComponent",
+    name: "testComponent",
+    component: () => import("../components/testComponents/steps.vue"),
   },
-  {
-    path: '/dashboard',
-    name: 'Dashboard',
-    component: () => import('../views/Dashboard.vue'),
-    meta: { authOnly: true},
-
-
-  },
-  {
-    path: '/register',
-    name: 'Register',
-    meta: {guestOnly: true},
-    component: () => import('../views/Register.vue')
-  },
-  {
-    path: '/verify-email',
-    name: 'verifyEmail',
-    meta: {guestOnly: true},
-    component: () => import('../components/auth/VerifyEmail.vue'),
-  },
-  {
-    path: '/forgot-password',
-    name: 'forgotPassword',
-    meta: {guestOnly: true},
-    component: () => import('../components/auth/ForgotPassword.vue'),
-  },
-  {
-    path: '/reset-password',
-    name: 'resetPassword',
-    meta: {guestOnly: true},
-    component: () => import('../components/auth/ResetPassword.vue'),
-  },
-  {
-    path: '/payment',
-    name: 'PaymentResult',
-    meta: {authOnly: true},
-    component: () => import('../components/payment/PaymentResult.vue'),
-  },
-  {
-    path: '/reservations',
-    name: 'Reservations',
-    meta: {authOnly: true},
-    component: () => import('../components/auth/Reservations.vue'),
-  },
-  { path: '*',
-    name: 'notFound' ,
-    component: NotFound }
-]
+];
 
 const router = new VueRouter({
   routes,
-  mode: 'history'
-})
+  mode: "history",
+});
 
 function isLoggedIn() {
-  return store.getters.authenticated
+  return store.getters.authenticated;
 }
 function isCalculated() {
-  return localStorage.getItem("calculated")
+  return localStorage.getItem("calculated");
 }
 
 router.beforeEach((to, from, next) => {
-  if (to.matched.some(record => record.meta.authOnly)) {
+  if (to.matched.some((record) => record.meta.authOnly)) {
     // this route requires auth, check if logged in
     // if not, redirect to login page.
     if (!isLoggedIn()) {
       next({
-        path: '/login',
-        query: { redirect: to.fullPath }
-      })
+        path: "/login",
+        query: { redirect: to.fullPath },
+      });
     } else {
-      next()
+      next();
     }
-  } else if (to.matched.some(record => record.meta.guestOnly)) {
+  } else if (to.matched.some((record) => record.meta.guestOnly)) {
     // this route requires auth, check if logged in
     // if not, redirect to login page.
     if (isLoggedIn()) {
       next({
-        path: '/dashboard',
-        query: { redirect: to.fullPath }
-      })
+        path: "/dashboard",
+        query: { redirect: to.fullPath },
+      });
     } else {
-      next()
+      next();
     }
   } else {
-    next() // make sure to always call next()!
+    next(); // make sure to always call next()!
   }
-})
+});
 
 router.beforeEach((to, from, next) => {
-  if (to.matched.some(record => record.meta.CalculatedOnly)) {
+  if (to.matched.some((record) => record.meta.CalculatedOnly)) {
     if (!isCalculated()) {
       next({
-        path: '/',
-        query: { redirect: to.fullPath }
-      })
+        path: "/",
+        query: { redirect: to.fullPath },
+      });
     } else {
-      next()
+      next();
     }
   } else {
-    next() // make sure to always call next()!
+    next(); // make sure to always call next()!
   }
-})
+});
 
-export default router
+export default router;
