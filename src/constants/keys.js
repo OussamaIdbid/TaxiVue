990b0fc0bcdf3e6c7c9ee2e57816c11b73aaedad
OussamaIdbid <57132225+OussamaIdbid@users.noreply.github.com>
2021-03-15 20:30:56 +0100

    added reusable constants for more coherent code

---
+++ src/constants/keys.js
@@ -0,0 +1,3 @@
+export const DecryptKey = '4MkN8BfGRpNwgUz'
+export const HereApiKey = '4L8Nsl9VR_O3VV-MxXIlujMALfBGteJLT-6q25X4VuI'
+export const MapBoxKey = 'pk.eyJ1Ijoib2lkYmlkIiwiYSI6ImNrZGJ0c29kMDAzdnQyc3FxcGRlaHR0aXEifQ.MLtBfQB6Hctj-y3FDuuckg'
---
+++ src/constants/mapBox/BaseRequests.js
@@ -0,0 +1,3 @@
+export const SEARCH_API_BASE = 'https://api.mapbox.com/geocoding/v5/mapbox.places/'
+export const DIRECTIONS_API_BASE ='https://api.mapbox.com/directions/v5/mapbox/driving/'
+
