9fddf2cd23f3ca8f0f7b4d52dbd92cd5dc2c4cef
OussamaIdbid <57132225+OussamaIdbid@users.noreply.github.com>
2021-03-22 22:47:49 +0100

    Added constant to farecalculation file

--- src/constants/fareCalculations.js
+++ src/constants/fareCalculations.js
@@ -6,6 +6,11 @@ export const CALCULATION_CONSTANTS = {
      PER_MIN_ONE_TO_FOUR: 0.4,
      PER_MIN_FOUR_TO_SEVEN: 0.45,
 }
+export const PERSON_CATEGORIES = [
+     { id: 1, name: "1-4 personen" },
+     { id: 2, name: "4-7 personen" },
+]
+
 
 
 
