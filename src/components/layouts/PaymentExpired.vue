b475542115ff2587c6d633a684f2b056414882c4
OussamaIdbid <57132225+OussamaIdbid@users.noreply.github.com>
2021-06-18 10:40:35 +0200

    added error page if payment is expired

---
+++ src/components/layouts/PaymentExpired.vue
@@ -0,0 +1,45 @@
+<template>
+    <div>
+        <section class="section is-large">
+            <div class="container">
+                <div class="columns is-centered">
+                    <div class="column is-one-third">
+                        Betaling is niet geldig     
+                    </div>
+                    <div class="column is-one-third ">
+                        <b-button type="is-danger" size="is-medium" @click="toHome" outlined>Terug naar Home</b-button>
+                    </div>
+                </div>
+            </div>
+
+        </section>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "NotFound",
+        methods: {
+            toHome() {
+                this.$router.push({name: "Home"})
+            }
+    },
+    mounted () {
+        window.scrollTo(0, 0)
+        },
+
+    }
+</script>
+
+<style scoped>
+    .column {
+        text-align: center;
+        color: #f14668;
+        font-size: xx-large;
+    }
+    .columns {
+        flex-direction: column;
+        align-items: center;
+
+    }
+</style>
