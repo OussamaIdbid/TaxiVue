e1232537e24ff20423743eb937592ce9a8e43342
OussamaIdbid <57132225+OussamaIdbid@users.noreply.github.com>
2021-06-20 04:25:11 +0200

    added discount design and logic

--- src/components/reservation/StepFinal.vue
+++ src/components/reservation/StepFinal.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="container">
     <div class="box">
-      <b-field label="Eindbestemming" label-position="on-border">
+      <b-field label="Vertrekpunt" label-position="on-border">
         <b-input
           disabled
           v-model="reservation.reservation.StartObject.place_name"
@@ -52,21 +52,62 @@
         </b-input>
       </b-field>
     </div>
+    <div class="box">
+      <div class="column-info">
+        <span class="material-icons"> euro </span>
+        <div v-if="!isLoading" class="info-end-wrapper">
+          <p class="label">Subtotaal</p>
+          <p class="info-end">€{{ reservation.reservation.farePrice }}</p>
+        </div>
+      </div>
+      <div v-if="!discountIsCalculated" class="column-info">
+        <span class="material-icons"> local_offer </span>
+        <b-field label="Korting" label-position="on-border">
+          <b-input v-model="discountCode" placeholder="Vul een kortingscode in">
+          </b-input>
+        </b-field>
+        <b-button
+          :disabled="DiscountButtonIsDisabled"
+          @click="getDiscount"
+          type="is-danger"
+          >Toepassen</b-button
+        >
+      </div>
+      <div v-else-if="discountIsCalculated" class="column-info">
+        <span class="material-icons"> local_offer </span>
+        <div v-if="!isLoading" class="info-end-wrapper">
+          <p class="label">Korting</p>
+          <div class="info-wrapper">
+            <p class="info-end">-€{{ discount }}</p>
+            <b-button @click="resetDiscount" size="is-small" type="is-danger"
+              >Verwijder korting</b-button
+            >
+          </div>
+        </div>
+      </div>
+      <div class="column-info">
+        <span class="material-icons"> euro </span>
+        <div v-if="!isLoading" class="info-end-wrapper">
+          <p class="label">Totaal</p>
+          <p class="info-end">€{{ totalPrice }}</p>
+        </div>
+      </div>
 
-    <div>Total: €{{ reservation.reservation.farePrice }}</div>
-    <button
-      id="cancel-edit-fare"
-      class="button is-danger has-text-centered"
-      @click="handlePayment()"
-    >
-      Naar Betalen
-    </button>
+      <button
+        id="cancel-edit-fare"
+        class="button is-danger has-text-centered"
+        @click="handlePayment()"
+      >
+        Naar Betalen
+      </button>
+    </div>
   </div>
 </template>
 
 <script>
 import { mapGetters } from "vuex";
-import Reservation from './../../Api/Reservation'
+import Reservation from "./../../Api/Reservation";
+import Discount from "./../../Api/Discount";
 export default {
   computed: {
     ...mapGetters("CurrentReservation", ["reservation", "userDetails"]),
@@ -80,6 +121,11 @@ export default {
       phoneNumber: "",
       time: null,
       date: null,
+      discount: 0,
+      totalPrice: 0,
+      discountCode: "",
+      discountIsCalculated: false,
+      DiscountButtonIsDisabled: true,
     };
   },
   mounted() {
@@ -89,13 +135,60 @@ export default {
     this.time = new Date(this.reservation.userDetails.time);
 
     this.date = new Date(this.reservation.userDetails.date);
+    console.log(typeof this.reservation.reservation.farePrice);
+    (this.totalPrice = this.reservation.reservation.farePrice), 10;
+  },
+  watch: {
+    discountCode: function (val) {
+      val.length > 0
+        ? (this.DiscountButtonIsDisabled = false)
+        : (this.DiscountButtonIsDisabled = true);
+    },
   },
   methods: {
     handlePayment() {
       console.log(this.reservation.reservation.farePrice);
-      Reservation.Payment(this.reservation.reservation.farePrice).then((response) => {
-        window.open(response.data, "_self");
-      });
+      Reservation.Payment(this.reservation.reservation.farePrice).then(
+        (response) => {
+          window.open(response.data, "_self");
+        }
+      );
+    },
+    getDiscount() {
+      Discount.getDiscount(this.discountCode)
+        .then((response) => {
+          if (response.data.type === 0) {
+            this.discount = (
+              (this.reservation.reservation.farePrice / 100) *
+              response.data.value
+            ).toFixed(2);
+          } else if (response.data.type === 1) {
+            this.discount = response.data.value;
+          }
+
+          this.totalPrice = (
+            this.reservation.reservation.farePrice - this.discount
+          ).toFixed(2);
+          this.discountIsCalculated = true;
+        })
+        .catch((error) => {
+          console.log(error);
+          this.$buefy.snackbar.open({
+            type: "is-danger",
+            actionText: "OK",
+            position: "is-bottom",
+            message: "Kortingscode niet geldig",
+            duration: 1500
+          });
+
+          this.discountCode = "";
+        });
+    },
+    resetDiscount() {
+      this.discountCode = "";
+      this.discount = 0;
+      this.discountIsCalculated = false;
+      this.totalPrice = this.reservation.reservation.farePrice
     },
   },
 };
@@ -112,5 +205,23 @@ export default {
 <style scoped>
 .datepicker {
   display: block;
+}
+.label {
+  font-size: large;
+  margin-bottom: 0;
+}
+.material-icons {
+  color: #f14668;
+  margin-right: 1rem;
+}
+.column-info {
+  display: flex;
+  margin-bottom: 1rem;
+}
+.info-wrapper {
+  display: flex;
+}
+.info-end {
+  margin-right: 1rem;
 }
 </style>
