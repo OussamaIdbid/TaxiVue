16a580a057831cd7aa3c6e2811dd63e010c29900
OussamaIdbid <57132225+OussamaIdbid@users.noreply.github.com>
2021-06-26 13:16:24 +0200

    added seperate file with default state

---
+++ src/Store/defaultState/currentReservation.js
@@ -0,0 +1,13 @@
+export const defaultState = {
+    reservation: [],
+    userDetails: {
+      date: null,
+      time: null,
+      phonenumber: "",
+    },
+    isCalculated: false,
+    nextIsDisabled: false,
+    previousIsVisible: false,
+    nextIsVisible: true,
+    currentStep: 0,
+  };
