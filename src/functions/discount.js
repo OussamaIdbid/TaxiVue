b57162eb2eb003e40e8aeccc42e3d8df52dddca4
OussamaIdbid <57132225+OussamaIdbid@users.noreply.github.com>
2021-06-27 16:34:06 +0200

    format

--- src/functions/discount.js
+++ src/functions/discount.js
@@ -1,25 +1,25 @@
 /* eslint-disable */
 import Discount from "./../Api/Discount";
-import moment from 'moment'
+import moment from "moment";
 
 export const discountIsValid = (userId, discount) =>
-  new Promise( async(resolve) => {
+  new Promise(async (resolve) => {
     if (!moment().isBefore(moment(discount.expire_date)))
       return resolve({
         success: false,
         errorMsg: "Kortingscode niet meer geldig",
       });
 
-    const {data} = await Discount.getDiscountUser();
-    
-    if(data.length === 0) return resolve({success: true})
+    const { data } = await Discount.getDiscountUser();
+
+    if (data.length === 0) return resolve({ success: true });
 
     const usedDiscount = data.filter(
       (userDiscount) =>
         userDiscount.user_id === userId &&
         userDiscount.discount_id === discount.id
     );
-    
+
     if (usedDiscount !== undefined)
       return resolve({
         success: false,
@@ -28,4 +28,3 @@ export const discountIsValid = (userId, discount) =>
 
     return resolve({ success: true });
   });
-
